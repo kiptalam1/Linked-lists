2301b09ac3253e6770c43ff7e11214382ea81ea3
adams <adamskiptalam0@gmail.com>
2025-01-17 12:28:06 +0300

    Initial linked list

---
+++ linkedLists.js
@@ -0,0 +1,16 @@
+class Node {
+    constructor(value) {
+        this.value = null
+        this.next = null
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null
+        this.length = 0
+    }
+}
+
+let list = new LinkedList();
+console.log(list)
