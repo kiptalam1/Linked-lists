15ffd99667caf544c0e53a376a6b86f3e71693c5
adams <adamskiptalam0@gmail.com>
2025-01-17 16:22:46 +0300

    Add at(index), pop(), and contains(value) methods to the linked list.

--- linkedLists.js
+++ linkedLists.js
@@ -9,6 +9,7 @@ class LinkedList {
     constructor() {
         this.head = null
         this.length = 0
+        this.array = [];
     }
     append(value) {
         let node = new Node(value);
@@ -23,6 +24,7 @@ class LinkedList {
             }
             current.next = node;
         }
+        this.array.push(node);
         this.length++
     }
     prepend(value) {
@@ -33,10 +35,11 @@ class LinkedList {
             node.next = this.head;
             this.head = node;
         }
+        this.array.unshift(node);
         this.length++
     }
     size() {
-        return this.length;
+        return this.array.length;
     }
     getHead() {
         // returns first node of the list.
@@ -50,6 +53,33 @@ class LinkedList {
         }
         return current;
     }
+    at(index) {
+        if (this.head === null) return 'List is empty';
+        let arr = this.array;
+        if (index < 0 || index > arr.length) return 'Index is out of range';
+        else {
+            let node = arr[index];
+            return node;
+        }
+    }
+    pop() {
+        if (this.head === null) return 'List is empty';
+        let current = this.head;
+        while (current.next.next !== null) {
+            current = current.next;
+        }
+        current.next = null;
+        this.array.pop();
+        this.length--;
+    }
+    contains(value) {
+        let current = this.head;
+        while (current !== null) {
+            if (current.value === value) return true;
+            current = current.next;
+        }
+        return false;
+    }
 }
 
 let list = new LinkedList();
@@ -60,9 +90,14 @@ list.append(10)
 list.append(12)
 list.append(17)
 list.append(39)
+list.prepend(1)
+//list.pop();
+
 
 
 console.log(list)
 console.log("Size: ", list.size())
-console.log("Head: ", list.getHead())
-console.log("Tail: ", list.tail())
+//console.log("Head: ", list.getHead())
+//console.log("Tail: ", list.tail())
+//console.log("At: ", list.at(0))
+console.log(list.contains(6))
