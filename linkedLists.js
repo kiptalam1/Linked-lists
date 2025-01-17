898532190612d4e3e38fd937bf8280b0dfc6e2be
adams <adamskiptalam0@gmail.com>
2025-01-17 14:45:31 +0300

    Add append, prepend, size, getHead, tail methods to the linked list.

--- linkedLists.js
+++ linkedLists.js
@@ -10,7 +10,59 @@ class LinkedList {
         this.head = null
         this.length = 0
     }
+    append(value) {
+        let node = new Node(value);
+        if (node.value === null) {
+            node.value = value;
+        }
+        if (this.head === null) this.head = node;
+        else{
+            let current = this.head
+            while (current.next !== null) {
+                current = current.next;
+            }
+            current.next = node;
+        }
+        this.length++
+    }
+    prepend(value) {
+        let node = new Node(value);
+        if (node.value === null) node.value = value;
+        if (this.head === null) this.head = node;
+        else {
+            node.next = this.head;
+            this.head = node;
+        }
+        this.length++
+    }
+    size() {
+        return this.length;
+    }
+    getHead() {
+        // returns first node of the list.
+        return this.head;
+    }
+    tail() {
+        if (this.head === null) return null;
+        let current = this.head;
+        while (current.next !== null) {
+            current = current.next;
+        }
+        return current;
+    }
 }
 
 let list = new LinkedList();
+list.prepend(9)
+list.append(5)
+list.prepend(6)
+list.append(10)
+list.append(12)
+list.append(17)
+list.append(39)
+
+
 console.log(list)
+console.log("Size: ", list.size())
+console.log("Head: ", list.getHead())
+console.log("Tail: ", list.tail())
